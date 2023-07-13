ca18b367dd1a2dfae2ca1b17a0816d5b351a0f76
Suazo3000 <suazosteve1@gmail.com>
2023-07-12 20:38:36 -0500

    created the vars for charaters

---
+++ script.js
@@ -0,0 +1,7 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var upper_Case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower_Case = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var special = "!@,#$%&*{}[]+\/=";
+var chosenCharacters = "";
